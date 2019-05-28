3d429ece5b22bd064391b7b038d61d737acd65b6
JeanMoraes <moraes.jean91@gmail.com>
2019-05-28 15:33:14 -0300

    criação da página de para cadastro de funcionários

--- adm/build/template_partes/footer.php
+++ adm/build/template_partes/footer.php
@@ -8,7 +8,7 @@
         <!-- /footer content -->
       </div>
     </div>
-
+    
     <!-- jQuery -->
     <script src="vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
@@ -17,25 +17,31 @@
     <script src="vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
     <script src="vendors/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="vendors/Chart.js/dist/Chart.min.js"></script>
-    <!-- jQuery Sparklines -->
-    <script src="vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-    <!-- Flot -->
-    <script src="vendors/Flot/jquery.flot.js"></script>
-    <script src="vendors/Flot/jquery.flot.pie.js"></script>
-    <script src="vendors/Flot/jquery.flot.time.js"></script>
-    <script src="vendors/Flot/jquery.flot.stack.js"></script>
-    <script src="vendors/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="vendors/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="vendors/DateJS/build/date.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="vendors/iCheck/icheck.min.js"></script>
     <!-- bootstrap-daterangepicker -->
     <script src="vendors/moment/min/moment.min.js"></script>
     <script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- bootstrap-wysiwyg -->
+    <script src="vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js"></script>
+    <script src="vendors/jquery.hotkeys/jquery.hotkeys.js"></script>
+    <script src="vendors/google-code-prettify/src/prettify.js"></script>
+    <!-- jQuery Tags Input -->
+    <script src="vendors/jquery.tagsinput/src/jquery.tagsinput.js"></script>
+    <!-- Switchery -->
+    <script src="vendors/switchery/dist/switchery.min.js"></script>
+    <!-- Select2 -->
+    <script src="vendors/select2/dist/js/select2.full.min.js"></script>
+    <!-- Parsley -->
+    <script src="vendors/parsleyjs/dist/parsley.min.js"></script>
+    <!-- Autosize -->
+    <script src="vendors/autosize/dist/autosize.min.js"></script>
+    <!-- jQuery autocomplete -->
+    <script src="vendors/devbridge-autocomplete/dist/jquery.autocomplete.min.js"></script>
+    <!-- starrr -->
+    <script src="vendors/starrr/dist/starrr.js"></script>
 
     <!-- Datatables -->
     <script src="vendors/datatables.net/js/jquery.dataTables.min.js"></script>
@@ -53,8 +59,9 @@
     <script src="vendors/jszip/dist/jszip.min.js"></script>
     <script src="vendors/pdfmake/build/pdfmake.min.js"></script>
     <script src="vendors/pdfmake/build/vfs_fonts.js"></script>
-    
+
     <!-- Custom Theme Scripts -->
-    <script src="build/js/custom.js"></script>
+    <script src="build/js/custom.min.js"></script>
+    
   </body>
 </html>
--- adm/cadastrar_usuarios.php
+++ adm/cadastrar_usuarios.php
@@ -27,97 +27,90 @@
         <div class="right_col" role="main">
           <div class="">
 
-            <div class="row">
-              <div class="col-md-4">
+          <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Status Verde <small>Últimos Registros</small></h2>
+                    <h2>Form Design <small>different form elements</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                    <?php
-                    if($total_verde > 0) {
-                    do {
-                    ?>  
-                    <article class="media event">
-                      <a class="pull-left border-aero profile_thumb" style="background-image: url('imagens/perfil/<?=$linha_verde['foto']?>'); background-size: cover;">
-                        
-                      </a>
-                      <div class="media-body">
-                        <a class="title" href="#"><?=$linha_verde['nome']?></a>
-                        <p><strong>Função:</strong> <?=$linha_verde['funcao']?></p>
-                        <p><strong>Telefone:</strong> <?=$linha_verde['telefone']?></p>
-                        <?php echo "<a href='registro-detalhes.php?id=" . $linha_verde['id'] . "'>Detalhes</a>"; ?>
-                      </div>
-                    </article>
-                    <?php
-                       }while($linha_verde = mysqli_fetch_assoc($sql_verde));
-                       }
-                     ?>
-                  </div>
-                </div>
-              </div>
+                    <br />
+                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
 
-              <div class="col-md-4">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Status Amarelo <small>Últimos Registros</small></h2>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                  <?php
-                    if($total_amarelo > 0) {
-                    do {
-                    ?>  
-                    <article class="media event">
-                      <a class="pull-left border-aero profile_thumb" style="background-image: url('imagens/perfil/<?=$linha_amarelo['foto']?>'); background-size: cover;">
-                        
-                      </a>
-                      <div class="media-body">
-                        <a class="title" href="#"><?=$linha_amarelo['nome']?></a>
-                        <p><strong>Função:</strong> <?=$linha_amarelo['funcao']?></p>
-                        <p><strong>Telefone:</strong> <?=$linha_amarelo['telefone']?></p>
-                        <?php echo "<a href='registro-detalhes.php?id=" . $linha_amarelo['id'] . "'>Detalhes</a>"; ?>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nome <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
                       </div>
-                    </article>
-                    <?php
-                       }while($linha_amarelo = mysqli_fetch_assoc($sql_amarelo));
-                       }
-                     ?>
-                  </div>
-                </div>
-              </div>
 
-              <div class="col-md-4">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Status Vermelho <small>Últimos Registros</small></h2>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                  <?php
-                    if($total_vermelho > 0) {
-                    do {
-                    ?>  
-                    <article class="media event">
-                      <a class="pull-left border-aero profile_thumb" style="background-image: url('imagens/perfil/<?=$linha_vermelho['foto']?>'); background-size: cover;">
-                        
-                      </a>
-                      <div class="media-body">
-                        <a class="title" href="#"><?=$linha_vermelho['nome']?></a>
-                        <p><strong>Função:</strong> <?=$linha_vermelho['funcao']?></p>
-                        <p><strong>Telefone:</strong> <?=$linha_vermelho['telefone']?></p>
-                        <?php echo "<a href='registro-detalhes.php?id=" . $linha_vermelho['id'] . "'>Detalhes</a>"; ?>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Email <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="email" id="email" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
                       </div>
-                    </article>
-                    <?php
-                       }while($linha_vermelho = mysqli_fetch_assoc($sql_vermelho));
-                       }
-                     ?>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Senha <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="password" id="senha" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="col-md-3 col-sm-3 col-xs-12 control-label">Nível de Acesso
+                          <br>
+                          <small class="text-navy">Normal Bootstrap elements</small>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <div class="radio">
+                            <label>
+                              <input type="radio" class="flat" checked name="iCheck"> Monitor
+                            </label>
+                          </div>
+                          <div class="radio">
+                            <label>
+                              <input type="radio" class="flat" name="iCheck"> Administrador
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button type="submit" class="btn btn-success">Cadastrar</button>
+                        </div>
+                      </div>
+
+                    </form>
+                    
                   </div>
                 </div>
               </div>
             </div>
+
+
           </div>
         </div>
         <!-- /page content -->
